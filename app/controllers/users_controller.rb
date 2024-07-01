c88b4a56f46a30bfbb7d44bb4d7d5357824b032a
João Lopes <joao.pls.lopes@gmail.com>
2024-07-01 14:58:35 +0100

    - Created edit account page with route and actions  - Changed text on the user form so it changes from "Sign up" to "Update account" depending if its a new record or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
+  def edit
+  end
 
+  def create
     if @user.save
       flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up."
       redirect_to articles_path
     else
       render 'new', status: :unprocessable_content
     end
+  end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account was succesfully updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
