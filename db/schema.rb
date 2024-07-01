4bd5598fc167c98b7126547a89cc30c3007d87e1
João Lopes <joao.pls.lopes@gmail.com>
2024-07-01 12:28:49 +0100

    - Added bcrypt to the gemfile  - Added column "password_digest" to store the hashed password from bcrypt  - Enabled has_secure_password functionality to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_110751) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_112359) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_01_110751) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
